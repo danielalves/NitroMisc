bfcd9262743f14935ff30522b41afd6e92ffc577
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-07-18 17:04:04 -0300

    Adding podfile

---
+++ NitroMisc/NitroMiscTests/NTRMacrosTests.m
@@ -0,0 +1,46 @@
+//
+//  NTRMacrosTests.m
+//  NitroMiscTests
+//
+//  Created by Daniel L. Alves on 18/7/14.
+//  Copyright (c) 2014 Daniel L. Alves. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+// NitroMisc
+#import "NTRMacros.h"
+
+#pragma mark - Interface
+
+@interface NTRMacrosTests : XCTestCase
+{
+    SEL aSelector;
+}
+@end
+
+#pragma mark - Implementation
+
+@implementation NTRMacrosTests
+
+-( void )test_SuppressPerformSelectorLeakWarning_suppresses_warning
+{
+    // Turns "performSelector may cause a leak because its selector is unknown" warning
+    // into an error
+#pragma clang diagnostic push
+#pragma clang diagnostic error "-Warc-performSelector-leaks"
+    
+    // If we have no build errors, thats because SuppressPerformSelectorLeakWarning
+    // worked and suppressed the warning (which is an error for now)
+    NSObject *object;
+    if( [object respondsToSelector: aSelector] )
+        SuppressPerformSelectorLeakWarning( [object performSelector: aSelector] );
+    
+#pragma clang diagnostic pop
+    
+    // If this method compiles, our test will have passed. That's
+    // why we use this XCTAssertTrue( YES )
+    XCTAssertTrue( YES );
+}
+
+@end
