3d60402e4325a62da9c9a8edb68df0cf4a6c18d8
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-07-18 17:01:29 -0300

    Adding more tests for NTR_LOG* macros

--- NitroMisc/NitroMiscTests/NitroNSStringLoggerTests.m
+++ NitroMisc/NitroMiscTests/NitroNSStringLoggerTests.m
@@ -1,6 +1,6 @@
 //
 //  NitroNSStringLoggerTests.m
-//  NitroNSStringLoggerTests
+//  NitroMiscTests
 //
 //  Created by Daniel L. Alves on 17/07/14.
 //  Copyright (c) 2014 Daniel L. Alves. All rights reserved.
@@ -9,18 +9,22 @@
 #import <XCTest/XCTest.h>
 
 // NitroMisc
+#import "fmemopen.h"
 #import "NSString+Logger_Nitro.h"
 
 #pragma mark - Interface
 
 @interface NitroNSStringLoggerTests : XCTestCase
+{
+    SEL aSelector;
+}
 @end
 
 #pragma mark - Implementation
 
 @implementation NitroNSStringLoggerTests
 
--( void )NTR_LOG_compiles_with_constant_string_format_and_no_ellipsis_paramaterers
+-( void )test_NTR_LOG_compiles_with_constant_string_format_and_no_ellipsis_paramaterers
 {
     NTR_LOG( @"Hello!" );
     
@@ -29,7 +33,7 @@
     XCTAssertTrue( YES );
 }
 
--( void )NTR_LOG_compiles_with_constant_string_format_and_ellipsis_paramaterers
+-( void )test_NTR_LOG_compiles_with_constant_string_format_and_ellipsis_paramaterers
 {
     NTR_LOG( @"Hello! %@ %@, would you like %d as always?", @"Mr.", @"Homer", 3 );
     
@@ -38,4 +42,102 @@
     XCTAssertTrue( YES );
 }
 
+-( void )test_NTR_LOG_logs_message
+{
+    [self assertLoggedMessage: @"test" afterRunningLogMacroInsideBlock: ^{ NTR_LOG( @"test" ); }];
+    [self assertLoggedMessage: @"test 1, 2, 3" afterRunningLogMacroInsideBlock: ^{ NTR_LOG( @"test %d, %d, %d", 1, 2, 3 ); }];
+}
+
+-( void )test_NTR_LOGI_prepends_INFO_string
+{
+    [self assertLoggedMessage: @"INFO: test" afterRunningLogMacroInsideBlock: ^{ NTR_LOGI( @"test" ); }];
+}
+
+-( void )test_NTR_LOGW_prepends_WARNING_string
+{
+    [self assertLoggedMessage: @"WARNING: test" afterRunningLogMacroInsideBlock: ^{ NTR_LOGW( @"test" ); }];
+}
+
+-( void )test_NTR_LOGE_prepends_ERROR_string
+{
+    [self assertLoggedMessage: @"ERROR: test" afterRunningLogMacroInsideBlock: ^{ NTR_LOGE( @"test" ); }];
+}
+
+#pragma mark - Helpers
+
+-( void )assertLoggedMessage:( NSString * )message afterRunningLogMacroInsideBlock:( void (^)( void ))logMacroBlock
+{
+    NSString *documentsFolderPath = [NSSearchPathForDirectoriesInDomains( NSDocumentDirectory, NSUserDomainMask, YES ) lastObject];
+    NSString *filePath = [documentsFolderPath stringByAppendingPathComponent: @"test.txt"];
+    
+    const char *lowlevelFilePath = [[NSFileManager defaultManager] fileSystemRepresentationWithPath: filePath];
+    FILE *test = fopen( lowlevelFilePath, "w+" );
+    if( !test )
+    {
+        XCTFail( @"Could not create helper file" );
+        return;
+    }
+    
+    int stderrCopy = dup( STDERR_FILENO );
+    dup2( fileno(test),fileno( stderr ));
+    
+    logMacroBlock();
+    
+    fclose( test );
+    dup2( stderrCopy, STDERR_FILENO );
+    close( stderrCopy );
+    
+    NSError *error = nil;
+    NSString *str = [NSString stringWithContentsOfFile: filePath encoding: NSUTF8StringEncoding error: &error];
+    if( error )
+    {
+        XCTFail( @"Could not read helper file" );
+        return;
+    }
+
+    remove( lowlevelFilePath );
+
+    NSString *formattedSuffix = [NSString stringWithFormat: @"%@\n", message];
+    XCTAssertTrue( [str hasSuffix: formattedSuffix] );
+}
+
 @end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
