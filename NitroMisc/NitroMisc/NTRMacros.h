4014973365bb42c9315530438a09b784dffc9829
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-07-18 17:00:59 -0300

    Documenting NTRMacros file

--- NitroMisc/NitroMisc/NTRMacros.h
+++ NitroMisc/NitroMisc/NTRMacros.h
@@ -9,16 +9,28 @@
 #ifndef NITRO_MACROS_H
 #define NITRO_MACROS_H
 
-//
-// See:
-// http://stackoverflow.com/a/7933931
-//
-#define SuppressPerformSelectorLeakWarning(commands) \
-do { \
-_Pragma("clang diagnostic push") \
-_Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
-commands; \
-_Pragma("clang diagnostic pop") \
+/**
+ *  This macro suppresses the "performSelector may cause a leak because its selector is unknown" warnings
+ *  fired by performSelector* calls. This should only be used when you are sure the object responds to
+ *  the selector. Example:
+ *
+ *  -( void )letsFireASelectorWhoseNameWeDontKnow:( SEL )aSelector
+ *  {
+ *      if( [object respondsToSelector: aSelector] )
+ *          SuppressPerformSelectorLeakWarning( [object performSelector: aSelector] );
+ *  }
+ *
+ *  @param commands The command which makes the performSelector* call
+ *
+ *  @see http://stackoverflow.com/a/7933931
+ */
+#define SuppressPerformSelectorLeakWarning(commands)                    \
+do                                                                      \
+{                                                                       \
+    _Pragma("clang diagnostic push")                                    \
+    _Pragma("clang diagnostic ignored \"-Warc-performSelector-leaks\"") \
+    commands;                                                           \
+    _Pragma("clang diagnostic pop")                                     \
 } while(0)
 
 #endif
