e218182b24fb4f905d82e1025385ed48a64f8a97
Daniel L. Alves <alves.lopes.dan@gmail.com>
2014-07-22 16:17:11 -0300

    Improving documentation

--- NitroMisc/NitroMisc/NTRSynthesizeSingleton.h
+++ NitroMisc/NitroMisc/NTRSynthesizeSingleton.h
@@ -19,11 +19,55 @@
 
 #import <UIKit/UIKit.h>
 
-// To use in header files
+/*******************************************
+ *
+ * To use in header files
+ *
+ *******************************************/
+
+/**
+ *  Generates methods declarations needed to transform a common class into a singleton class.
+ *
+ *  @param className    The class for which singleton methods declarations will be created.
+ *  @param accessorname The accessor name used to obtain the singleton instance.
+ *
+ *  @see DEFAULT_DECLARE_SINGLETON_FOR_CLASS
+ *  @see SYNTHESIZE_SINGLETON_FOR_CLASS
+ *  @see DEFAULT_SYNTHESIZE_SINGLETON_FOR_CLASS
+ */
 #define DECLARE_SINGLETON_FOR_CLASS( className, accessorname ) +( className * )accessorname;
+
+/**
+ *  Generates methods declarations needed to transform a common class into a singleton class.
+ *  To obtain the singleton instance, call 'sharedInstance'.
+ *
+ *  Using this macro is the same as using DECLARE_SINGLETON_FOR_CLASS passing
+ *  'sharedInstance' as the singleton accessor name.
+ *
+ *  @param classname The class for which singleton methods declarations will be created.
+ *
+ *  @see DECLARE_SINGLETON_FOR_CLASS
+ *  @see SYNTHESIZE_SINGLETON_FOR_CLASS
+ *  @see DEFAULT_SYNTHESIZE_SINGLETON_FOR_CLASS
+ */
 #define DEFAULT_DECLARE_SINGLETON_FOR_CLASS( className ) DECLARE_SINGLETON_FOR_CLASS( className, sharedInstance )
 
-// To use in implementation files
+/*******************************************
+ *
+ * To use in implementation files
+ *
+ *******************************************/
+
+/**
+ *  Generates methods needed to transform a common class into a singleton class.
+ *
+ *  @param classname    The class for which singleton methods will be created.
+ *  @param accessorname The accessor name used to obtain the singleton instance.
+ *
+ *  @see DECLARE_SINGLETON_FOR_CLASS
+ *  @see DEFAULT_DECLARE_SINGLETON_FOR_CLASS
+ *  @see DEFAULT_SYNTHESIZE_SINGLETON_FOR_CLASS
+ */
 #define SYNTHESIZE_SINGLETON_FOR_CLASS( classname, accessorname )                                   \
                                                                                                     \
 static classname *shared##classname = nil;                                                          \
@@ -82,6 +126,19 @@ static classname *shared##classname = nil;                                      
     return self;                                                                                    \
 }
 
+/**
+ *  Generates methods needed to transform a common class into a singleton class.
+ *  To obtain the singleton instance, call 'sharedInstance'.
+ *
+ *  Using this macro is the same as using SYNTHESIZE_SINGLETON_FOR_CLASS passing
+ *  'sharedInstance' as the singleton accessor name.
+ *
+ *  @param classname The class for which singleton methods will be created.
+ *
+ *  @see DECLARE_SINGLETON_FOR_CLASS
+ *  @see DEFAULT_DECLARE_SINGLETON_FOR_CLASS
+ *  @see SYNTHESIZE_SINGLETON_FOR_CLASS
+ */
 #define DEFAULT_SYNTHESIZE_SINGLETON_FOR_CLASS( classname ) SYNTHESIZE_SINGLETON_FOR_CLASS( classname, sharedInstance )
 
 #endif
